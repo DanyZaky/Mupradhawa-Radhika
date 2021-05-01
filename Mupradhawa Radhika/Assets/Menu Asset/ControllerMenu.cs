5b2e2baa5951ecf51c310e6b07732c5b19008331
Dany Zaky <danyzakydany123@gmail.com>
2021-05-02 03:55:16 +0700

    main menu

---
+++ Mupradhawa Radhika/Assets/Menu Asset/ControllerMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerMenu : MonoBehaviour
+{
+    public GameObject playButton, gameLogo, settingButton, exitButton, backButton, chapter1Button, chapterScene;
+    public AudioSource hitButtonSound, backButtonSound;
+
+    public void tekanButtonPlay()
+    {
+        playButton.SetActive(false);
+        gameLogo.SetActive(false);
+        settingButton.SetActive(false);
+        exitButton.SetActive(false);
+
+        backButton.SetActive(true);
+        chapter1Button.SetActive(true);
+        chapterScene.SetActive(true);
+        hitButtonSound.Play();
+    }
+
+    public void tekanButtonBack()
+    {
+        playButton.SetActive(true);
+        gameLogo.SetActive(true);
+        settingButton.SetActive(true);
+        exitButton.SetActive(true);
+
+        backButton.SetActive(false);
+        chapter1Button.SetActive(false);
+        chapterScene.SetActive(false);
+        backButtonSound.Play();
+    }
+
+    public void tekanButtonSetting()
+    {
+        hitButtonSound.Play();
+    }
+
+    public void tekanButtonChapter1()
+    {
+        hitButtonSound.Play();
+    }
+}
