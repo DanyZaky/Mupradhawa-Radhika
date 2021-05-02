8ad56787fe35299a8e17370834975378c12cbaf6
unknown <pahry182@gmail.com>
2021-05-02 12:34:22 +0700

    Updated setting Windows

--- Mupradhawa Radhika/Assets/Menu Asset/ControllerMenu.cs
+++ Mupradhawa Radhika/Assets/Menu Asset/ControllerMenu.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class ControllerMenu : MonoBehaviour
 {
-    public GameObject playButton, gameLogo, settingButton, exitButton, backButton, chapter1Button, chapterScene;
-    public AudioSource hitButtonSound, backButtonSound;
+    public GameObject playButton, gameLogo, settingButton, exitButton, backButton, chapter1Button, chapterScene, settingWindow;
+    public AudioSource hitButtonSound, backButtonSound, bgmSound;
 
     public void tekanButtonPlay()
     {
@@ -33,13 +33,59 @@ public class ControllerMenu : MonoBehaviour
         backButtonSound.Play();
     }
 
-    public void tekanButtonSetting()
+    public void tekanOpenSetting()
     {
+        playButton.SetActive(false);
+        gameLogo.SetActive(false);
+        settingButton.SetActive(false);
+        exitButton.SetActive(false);
+
+        settingWindow.SetActive(true);
         hitButtonSound.Play();
+    }
+
+    public void tekanCloseSetting()
+    {
+        playButton.SetActive(true);
+        gameLogo.SetActive(true);
+        settingButton.SetActive(true);
+        exitButton.SetActive(true);
+
+        settingWindow.SetActive(false);
+        backButtonSound.Play();
     }
 
     public void tekanButtonChapter1()
     {
         hitButtonSound.Play();
     }
+
+    public void tekanButtonSFX()
+    {
+        hitButtonSound.Play();
+        if (hitButtonSound.volume == 1 || backButtonSound.volume == 1)
+        {
+            hitButtonSound.volume = 0;
+            backButtonSound.volume = 0;
+        }
+        else
+        {
+            hitButtonSound.volume = 1;
+            backButtonSound.volume = 1;
+        }
+    }
+
+    public void tekanButtonMusic()
+    {
+        hitButtonSound.Play();
+        if (bgmSound.volume != 0)
+        {
+            bgmSound.volume = 0;
+        } else
+        {
+            bgmSound.volume = 0.416f;
+        }
+        
+    }
+
 }
