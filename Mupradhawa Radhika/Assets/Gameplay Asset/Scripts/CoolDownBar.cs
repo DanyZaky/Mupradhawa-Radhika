cae0b78f2b8ec8b368db5e1b0e804ac8bf79e720
pahry182@gmail.com <pahry182@gmail.com>
2021-07-05 20:21:16 +0700

    revert

--- Mupradhawa Radhika/Assets/Gameplay Asset/Scripts/CoolDownBar.cs
+++ Mupradhawa Radhika/Assets/Gameplay Asset/Scripts/CoolDownBar.cs
@@ -6,34 +6,16 @@ using UnityEngine.UI;
 public class CoolDownBar : MonoBehaviour
 {
     public Slider timerSlider;
-    public float gameTime;
+    public int IndexCdCurrentEnemy;
+    public float[] CdEnemy;
 
     private BattleController bc;
     private bool stopTimer = true;
     private bool isEnemyAttack = false;
 
-    public Gradient gradient;
-    public Image fill;
-
     private void Awake()
     {
         bc = GetComponent<BattleController>();
-    }
-
-    public void InitiateCooldownBar()
-    {
-        stopTimer = false;
-        isEnemyAttack = false;
-        timerSlider.maxValue = gameTime;
-        timerSlider.value = gameTime;
-
-        fill.color = gradient.Evaluate(1f);
-    }
-
-    public void StartEnemyAttack()
-    {
-        StartCoroutine(bc.EnemyAttack());
-        fill.color = gradient.Evaluate(timerSlider.normalizedValue);
     }
 
     void Update()
@@ -41,11 +23,6 @@ public class CoolDownBar : MonoBehaviour
         if (stopTimer == false && bc.isEnemyAttackPaused == false)
         {
             timerSlider.value -= Time.deltaTime;
-        }
-        else
-        {
-            print(stopTimer);
-            print(bc.isEnemyAttackPaused);
         }
 
         if (timerSlider.value <= 0)
@@ -55,8 +32,16 @@ public class CoolDownBar : MonoBehaviour
             if (!isEnemyAttack)
             {
                 isEnemyAttack = true;
-                StartEnemyAttack();
+                bc.StartEnemyAttack();
             }
-        }        
+        }
     }
+
+    public void InitiateCooldownBar()
+    {
+        stopTimer = false;
+        isEnemyAttack = false;
+        timerSlider.maxValue = CdEnemy[IndexCdCurrentEnemy];
+        timerSlider.value = CdEnemy[IndexCdCurrentEnemy];
+    }   
 }
